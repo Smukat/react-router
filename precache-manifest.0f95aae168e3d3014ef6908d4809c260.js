4b1415cc0260be212198d88da42ca2ab65a46628
Jesica Prades <filaksofias@gmail.com>
2018-11-10 17:05:56 -0800

    Updates

---
+++ precache-manifest.0f95aae168e3d3014ef6908d4809c260.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d069c60c614401f74a6d",
+    "url": "/react-router/static/js/runtime~main.d069c60c.js"
+  },
+  {
+    "revision": "d117dc50b0303047ef56",
+    "url": "/react-router/static/js/main.d117dc50.chunk.js"
+  },
+  {
+    "revision": "c3ea707fa42674296da2",
+    "url": "/react-router/static/js/1.c3ea707f.chunk.js"
+  },
+  {
+    "revision": "d117dc50b0303047ef56",
+    "url": "/react-router/static/css/main.ddff8f53.chunk.css"
+  },
+  {
+    "revision": "28504ae451fba16ab679a49b83a200cd",
+    "url": "/react-router/index.html"
+  }
+];
